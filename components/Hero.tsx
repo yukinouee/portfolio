b4ac0b9be956577032544ec37acdaef0fd2f8dde
yukinouee <yuki2023@suou.waseda.jp>
2024-10-13 20:53:26 +0900

    1. Created Hero section. 2. Added "Hello." message to Hero section. 3. Applied Text Generate Effect (Aceternity UI) to the message.

---
+++ components/Hero.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { TextGenerateEffect } from './ui/text-generate-effect'
+
+const Hero = () => {
+  return (
+    <div className="text-center text-3xl justify-center mt-10">
+      <TextGenerateEffect
+        words="Hello."
+        />
+    </div>
+  )
+}
+
+export default Hero
